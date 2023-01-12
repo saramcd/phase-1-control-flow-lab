3969d9e9a34c08a250f84953744df947abc19972
Sara McDonnell <saramcdonnell32@gmail.com>
2023-01-11 20:27:41 -0500

    lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance < 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance >= 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let message;
+  city === "NYC" ? (message = "Ok, sounds good.") : (message = "No go.");
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let message;
+  switch (tip) {
+    case "generous":
+      message = "Thank you so much.";
+      break;
+    case "not as generous":
+      message = "Thank you.";
+      break;
+    default:
+      message = "Bye.";
+  }
+  return message;
 }
